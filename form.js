ad7cfd7cf7e450bc1f6d43ec4142569201444605
Kado-sleeper <simonkadnar@gmail.com>
2024-07-07 16:39:24 +0200

    fixed lights of buttons, added questions, clients, copyright, background-back.svg

--- form.js
+++ form.js
@@ -84,3 +84,24 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    const questions = document.querySelectorAll(".question");
+
+    questions.forEach(function(question) {
+        question.addEventListener("click", function() {
+            const answer = this.nextElementSibling;
+            const icon = this.querySelector(".toggle-icon");
+
+            // Toggle class for rotating icon
+            icon.classList.toggle("open");
+
+            // Toggle max-height to expand/collapse answer smoothly
+            if (answer.style.maxHeight) {
+                answer.style.maxHeight = null;
+            } else {
+                answer.style.maxHeight = answer.scrollHeight + "px";
+            }
+        });
+    });
+});

513ec17ab1bff337adde66fdf5c5f2832d3e028e
Kado-sleeper <simonkadnar@gmail.com>
2024-07-30 12:48:55 +0200

    added career and about us page

---
+++ lic.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const overlay = document.getElementById('fullscreenOverlay');
+    const fullscreenImage = document.getElementById('fullscreenImage');
+    
+    document.querySelectorAll('.zoom-image').forEach(image => {
+        image.addEventListener('click', function() {
+            // Nastaviť zdroj obrázka v overlay na ten, ktorý bol kliknutý
+            fullscreenImage.src = this.src;
+            overlay.classList.add('show');
+        });
+    });
+
+    overlay.addEventListener('click', function() {
+        overlay.classList.remove('show');
+    });
+});

f9cc9dfea9a674c63bfcd739055699010b7b09ee
Kado-sleeper <simonkadnar@gmail.com>
2024-08-06 16:15:26 +0200

    added background for form

--- form.js
+++ form.js
@@ -176,42 +176,42 @@ document.addEventListener('DOMContentLoaded', () => {
 
         if (title === 'Elektronická ochrana') {
             return `<div class="popup-form"> <h2>${title}</h2>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
             when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
             It has survived not only five centuries, but also the leap into electronic typesetting, 
             remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
-            Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
+            Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>`
             + `<img src="img/security.png" alt="Informátor">`
             + form + `</div>`;
         } else if (title === 'Fyzická ochrana osôb - bodyguard') {
             return `<div class="popup-form"> <h2>${title}</h2>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
             when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
             It has survived not only five centuries, but also the leap into electronic typesetting, 
             remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
-            Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
+            Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>`
             + `<img src="img/security.png" alt="Informátor">`
             + form + `</div>`;
         } else if (title === 'Fyzická ochrana v priestoroch zákazníka') {
             return `<div class="popup-form"> <h2>${title}</h2>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
             when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
             It has survived not only five centuries, but also the leap into electronic typesetting, 
             remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
-            Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
+            Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>`
             + `<img src="img/security.png" alt="Informátor">`
             + form + `</div>`;
         } else if (title === 'Hliadka pešej obchôdzky objektu') {
             return `<div class="popup-form"> <h2>${title}</h2>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
             when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
             It has survived not only five centuries, but also the leap into electronic typesetting, 
             remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing 
-            Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`
+            Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>`
             + `<img src="img/security.png" alt="Informátor">`
             + form + `</div>`;
         }

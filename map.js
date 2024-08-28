1f3e5d16a29456b891a97ae88d41077ab9afca05
Peter <polonipeto@gmail.com>
2024-08-28 13:39:39 +0200

    posledne upravy

--- map.js
+++ map.js
@@ -1,9 +1,6 @@
-// Initialize Leaflet map
-
 var slovakiaSouthWest = L.latLng(47.6, 16.8),
     slovakiaNorthEast = L.latLng(49.6, 22.6),
     slovakiaBounds = L.latLngBounds(slovakiaSouthWest, slovakiaNorthEast);
-
 
 var map = L.map('map', {
     center: [48.75, 19.5], // Centered approximately in Slovakia
@@ -28,80 +25,45 @@ var AlbasecIcon = L.icon({
     popupAnchor: [0, -32]
 });
 
-// Define the boundaries of Bratislava
-var southWest = L.latLng(48.1, 17.0),
-    northEast = L.latLng(48.2, 17.2),
-    bounds = L.latLngBounds(southWest, northEast);
-
-// Function to get random number within a range
-function getRandomNumber(min, max) {
-    return Math.random() * (max - min) + min;
-}
-
-// Function to add markers with custom icons
-function addMarkers(bounds, numberOfMarkers, icon, prefix) {
-    for (var i = 0; i < numberOfMarkers; i++) {
-        var lat = getRandomNumber(bounds.getSouthWest().lat, bounds.getNorthEast().lat);
-        var lng = getRandomNumber(bounds.getSouthWest().lng, bounds.getNorthEast().lng);
-        var companyName = prefix + (i + 1);
-        var marker = L.marker([lat, lng], { icon: icon }).addTo(map).bindPopup(companyName);
-        marker.on('mouseover', function (e) {
-            this.openPopup();
-        });
-        marker.on('mouseout', function (e) {
-            this.closePopup();
-        });
-    }
-}
-
-var bratislavaBounds = L.latLngBounds(L.latLng(48.1, 17.0), L.latLng(48.2, 17.2));
-addMarkers(bratislavaBounds, 20, AlbasecIcon, 'Company Bratislava');
-
-// Define the boundaries of Trnava
-var trnavaSouthWest = L.latLng(48.3, 17.5),
-    trnavaNorthEast = L.latLng(48.4, 17.7),
-    trnavaBounds = L.latLngBounds(trnavaSouthWest, trnavaNorthEast);
-
-// Generate random markers within Trnava
-var trnavaMarkers = [];
-var numberOfTrnavaMarkers = 10;
-
-for (var j = 0; j < numberOfTrnavaMarkers; j++) {
-    var trnavaLat = getRandomNumber(trnavaSouthWest.lat, trnavaNorthEast.lat);
-    var trnavaLng = getRandomNumber(trnavaSouthWest.lng, trnavaNorthEast.lng);
-    var companyName = 'Company Trnava' + (j + 1);
-    var trnavaMarker = L.marker([trnavaLat, trnavaLng]).addTo(map).bindPopup(companyName);
-    trnavaMarkers.push(trnavaMarker);
-    trnavaMarker.on('mouseover', function(e) {
+// Function to add a marker with a custom icon
+function addMarker(lat, lng, companyName, icon) {
+    var marker = L.marker([lat, lng], { icon: icon }).addTo(map).bindPopup(companyName);
+    marker.on('mouseover', function (e) {
         this.openPopup();
     });
-    trnavaMarker.on('mouseout', function(e) {
+    marker.on('mouseout', function (e) {
         this.closePopup();
     });
 }
 
-// Define the boundaries of Nitra
-var nitraSouthWest = L.latLng(48.3, 18.0),
-    nitraNorthEast = L.latLng(48.4, 18.2),
-    nitraBounds = L.latLngBounds(nitraSouthWest, nitraNorthEast);
+// Coordinates for the specified towns
+var towns = [
+    { name: 'Bratislava', lat: 48.1486, lng: 17.1077 },
+    { name: 'Senec', lat: 48.2195, lng: 17.3991 },
+    { name: 'Šaľa', lat: 48.1517, lng: 17.8729 },
+    { name: 'Sereď', lat: 48.2852, lng: 17.7265 },
+    { name: 'Senica', lat: 48.6792, lng: 17.3663 },
+    { name: 'Trenčín', lat: 48.8945, lng: 18.0444 },
+    { name: 'Poprad', lat: 49.0598, lng: 20.2975 },
+    { name: 'Prešov', lat: 48.9984, lng: 21.2394 },
+    { name: 'Dolný Kubín', lat: 49.2096, lng: 19.3033 },
+    { name: 'Trnava', lat: 48.3774, lng: 17.5872 },
+    { name: 'Košice', lat: 48.7164, lng: 21.2611 },
+    { name: 'Piešťany', lat: 48.5942, lng: 17.8265 },
+    { name: 'Prievidza', lat: 48.7745, lng: 18.6247 },
+    { name: 'Martin', lat: 49.0666, lng: 18.9228 },
+    { name: 'Liptovský Mikuláš', lat: 49.0842, lng: 19.6115 },
+    { name: 'Rovinka', lat: 48.0906, lng: 17.2231 },
+    { name: 'Hron', lat: 48.5802, lng: 19.1255 },
+    { name: 'Považská Bystrica', lat: 49.1222, lng: 18.4257 },
+    { name: 'Banská Bystrica', lat: 48.7395, lng: 19.1532 },
+    { name: 'Žilina', lat: 49.2231, lng: 18.7393 }
+];
 
-// Generate random markers within Nitra
-var nitraMarkers = [];
-var numberOfNitraMarkers = 10;
-
-for (var k = 0; k < numberOfNitraMarkers; k++) {
-    var nitraLat = getRandomNumber(nitraSouthWest.lat, nitraNorthEast.lat);
-    var nitraLng = getRandomNumber(nitraSouthWest.lng, nitraNorthEast.lng);
-    var companyName = 'Company Nitra' + (k + 1);
-    var nitraMarker = L.marker([nitraLat, nitraLng]).addTo(map).bindPopup(companyName);
-    nitraMarkers.push(nitraMarker);
-    nitraMarker.on('mouseover', function(e) {
-        this.openPopup();
-    });
-    nitraMarker.on('mouseout', function(e) {
-        this.closePopup();
-    });
-}
+// Add markers to the map for each town
+towns.forEach(function(town) {
+    addMarker(town.lat, town.lng, 'Company ' + town.name, AlbasecIcon);
+});
 
 map.once('click', function() {
     // Enable zoom control, dragging, double click zoom, and scroll wheel zoom

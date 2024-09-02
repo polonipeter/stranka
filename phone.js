6a1c63ddc6a5226f51b60eb747918c231bc0903a
Kado-sleeper <simonkadnar@gmail.com>
2024-09-02 14:10:10 +0200

    phone size

---
+++ phone.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburgerMenu').addEventListener('click', function () {
+    this.classList.toggle('active');
+    document.getElementById('navMenu').classList.toggle('active');
+});

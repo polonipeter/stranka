6c6033ab299740651d87696267df6950807f6720
Peter Poloni <polonipeto@gmail.com>
2024-10-25 19:57:55 +0200

    Remove files for deployment

--- phone.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('hamburgerMenu').addEventListener('click', function () {
-    this.classList.toggle('active');
-    document.getElementById('navMenu').classList.toggle('active');
-});
